c14a10fb0da1134e6f9b2df3090d9c6bc746cb11
luckysitara <udoessienprecious@gmail.com>
2026-01-22 08:00:49 -0800

    making it runnable

---
+++ app/src/main/java/app/vercel/reimagine_beta/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.reimagine_beta.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
